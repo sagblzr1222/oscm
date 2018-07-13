c5c293d522f93a75fd5a3683c014f7d4e28536e6
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2018-07-13 16:37:13 +0200

    Cleaned up the configuration

--- cxf_sts_tutorial/service/src/main/java/service/CountingServiceWS.java
+++ cxf_sts_tutorial/service/src/main/java/service/CountingServiceWS.java
@@ -4,11 +4,17 @@ import service.intf.CountingService;
 
 import javax.jws.WebService;
 
+import org.apache.cxf.annotations.EndpointProperties;
+import org.apache.cxf.annotations.EndpointProperty;
+
 @WebService(name = "CountingService",
             serviceName = "CountingService",
             targetNamespace = "http://oscm.org/xsd",
             endpointInterface = "service.intf.CountingService",
             wsdlLocation = "WEB-INF/wsdl/CountingService.wsdl")
+@EndpointProperties(value = {
+        @EndpointProperty(key = "ws-security.signature.properties", value = "serviceKeystore.properties"),
+        @EndpointProperty(key = "ws-security.callback-handler", value = "service.ServiceKeystorePasswordCallback") })
 public class CountingServiceWS implements CountingService {
 
     @Override
--- cxf_sts_tutorial/service/src/main/java/service/DoubleItPortTypeImpl.java
+++ cxf_sts_tutorial/service/src/main/java/service/DoubleItPortTypeImpl.java
@@ -2,13 +2,14 @@ package service;
 
 import javax.jws.WebService;
 
+import org.apache.cxf.annotations.EndpointProperties;
+import org.apache.cxf.annotations.EndpointProperty;
 import service.intf.DoubleItPortType;
 
-@WebService(targetNamespace = "http://www.example.org/contract/DoubleIt", 
-            portName="DoubleItPort",
-            serviceName="DoubleItService", 
-            endpointInterface="service.intf.DoubleItPortType",
-            wsdlLocation = "WEB-INF/wsdl/DoubleIt.wsdl")
+@WebService(targetNamespace = "http://www.example.org/contract/DoubleIt", portName = "DoubleItPort", serviceName = "DoubleItService", endpointInterface = "service.intf.DoubleItPortType", wsdlLocation = "WEB-INF/wsdl/DoubleIt.wsdl")
+@EndpointProperties(value = {
+        @EndpointProperty(key = "ws-security.signature.properties", value = "serviceKeystore.properties"),
+        @EndpointProperty(key = "ws-security.callback-handler", value = "service.ServiceKeystorePasswordCallback") })
 public class DoubleItPortTypeImpl implements DoubleItPortType {
 
     public int doubleIt(int numberToDouble) {
