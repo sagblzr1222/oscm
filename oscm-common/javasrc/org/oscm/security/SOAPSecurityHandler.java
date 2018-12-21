5d06fe6f15ccecbf3287e33673213f9bc64212a8
Lorenz Goebel <lorenzgoebel@gmx.de>
2018-12-21 01:03:51 +0100

    SOAP handler to correct message response element.

--- oscm-common/javasrc/org/oscm/security/SOAPSecurityHandler.java
+++ oscm-common/javasrc/org/oscm/security/SOAPSecurityHandler.java
@@ -8,23 +8,26 @@
 
 package org.oscm.security;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
-
 
 /**
  * @author stavreva
  *
  */
-public final class SOAPSecurityHandler implements SOAPHandler<SOAPMessageContext> {
-    
+public final class SOAPSecurityHandler
+        implements SOAPHandler<SOAPMessageContext> {
+
     private String userId;
     private String password;
 
@@ -33,45 +36,72 @@ public final class SOAPSecurityHandler implements SOAPHandler<SOAPMessageContext
         this.password = password;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public boolean handleMessage(final SOAPMessageContext msgCtx) {
 
         // Indicator telling us which direction this message is going in
-        final Boolean outInd = (Boolean) msgCtx.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        final Boolean outInd = (Boolean) msgCtx
+                .get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 
         // Handler must only add security headers to outbound messages
         if (outInd.booleanValue()) {
             try {
                 // Get the SOAP Envelope
-                final SOAPEnvelope envelope = msgCtx.getMessage().getSOAPPart().getEnvelope();
+                final SOAPEnvelope envelope = msgCtx.getMessage().getSOAPPart()
+                        .getEnvelope();
 
                 // Header may or may not exist yet
                 SOAPHeader header = envelope.getHeader();
                 if (header == null)
                     header = envelope.addHeader();
 
-                // Add WSS Usertoken Element Tree 
-                final SOAPElement security = header.addChildElement("Security", "wsse",
+                // Add WSS Usertoken Element Tree
+                final SOAPElement security = header.addChildElement("Security",
+                        "wsse",
                         "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
-                final SOAPElement userToken = security.addChildElement("UsernameToken", "wsse");
-                userToken.addChildElement("Username", "wsse").addTextNode(userId);
-                userToken.addChildElement("Password", "wsse").addAttribute(new QName("Type"), "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText").addTextNode(password);
+                final SOAPElement userToken = security
+                        .addChildElement("UsernameToken", "wsse");
+                userToken.addChildElement("Username", "wsse")
+                        .addTextNode(userId);
+                userToken.addChildElement("Password", "wsse").addAttribute(
+                        new QName("Type"),
+                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText")
+                        .addTextNode(password);
 
             } catch (final Exception e) {
                 e.printStackTrace();
                 return false;
             }
+        } else {
+            SOAPMessage message = msgCtx.getMessage();
+            try {
+                SOAPBody body = message.getSOAPBody();
+                Iterator<SOAPElement> bodyIterator = body.getChildElements();
+                SOAPElement responseElement = bodyIterator.next();
+                String resName = responseElement.getLocalName();
+                if (resName != null && !resName.endsWith("Response")) {
+                    resName = resName + "Response";
+                    responseElement.setElementQName(
+                            new QName(responseElement.getNamespaceURI(),
+                                    resName, responseElement.getPrefix()));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return true;
         }
         return true;
     }
-
 
     public String getUserId() {
         return userId;
     }
 
     /**
-     * @param userId the userId to set
+     * @param userId
+     *            the userId to set
      */
     public void setUserId(String userId) {
         this.userId = userId;
@@ -85,29 +115,38 @@ public final class SOAPSecurityHandler implements SOAPHandler<SOAPMessageContext
     }
 
     /**
-     * @param password the password to set
+     * @param password
+     *            the password to set
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    /* (non-Javadoc)
-     * @see javax.xml.ws.handler.Handler#close(javax.xml.ws.handler.MessageContext)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * javax.xml.ws.handler.Handler#close(javax.xml.ws.handler.MessageContext)
      */
     @Override
     public void close(MessageContext arg0) {
 
     }
 
-    /* (non-Javadoc)
-     * @see javax.xml.ws.handler.Handler#handleFault(javax.xml.ws.handler.MessageContext)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.xml.ws.handler.Handler#handleFault(javax.xml.ws.handler.
+     * MessageContext)
      */
     @Override
     public boolean handleFault(SOAPMessageContext arg0) {
         return false;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see javax.xml.ws.handler.soap.SOAPHandler#getHeaders()
      */
     @Override
