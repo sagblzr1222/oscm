ee09b1ab6ea9db05a2dd7c8447b660ef12999f21
Lorenz Goebel <lorenzgoebel@gmx.de>
2019-01-25 15:36:08 +0100

    Fixed unit tests.

--- oscm-app-vmware/javasrc/org/oscm/app/vmware/business/balancer/XMLHelper.java
+++ oscm-app-vmware/javasrc/org/oscm/app/vmware/business/balancer/XMLHelper.java
@@ -8,7 +8,14 @@
 
 package org.oscm.app.vmware.business.balancer;
 
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
 /**
  * @author goebel
@@ -16,17 +23,28 @@ import org.w3c.dom.Node;
  */
 class XMLHelper {
 
-    static String getAttributeValue(Node node, String name) {
+    protected static String getAttributeValue(Node node, String name) {
         return getAttributeValue(node, name, "");
     }
-    
-    
-    static String getAttributeValue(Node node, String name , String defaultValue) {
-        Node attr = node.getAttributes().getNamedItem("name");
+
+    protected static String getAttributeValue(Node node, String name,
+            String defaultValue) {
+        Node attr = node.getAttributes().getNamedItem(name);
         if (attr != null)
             return attr.getNodeValue();
         return defaultValue;
 
+    }
+
+    protected static Document convertToDocument(String string)
+            throws Exception {
+
+        DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
+        dfactory.setValidating(false);
+        dfactory.setIgnoringElementContentWhitespace(true);
+        DocumentBuilder builder = dfactory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(string)));
+        return doc;
     }
 
 }
